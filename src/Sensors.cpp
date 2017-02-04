6973c768820086000557ae8f8b6a751dd6c626fd
Arezki Issaadi <arezkiissaadi@gmail.com>
2017-02-04 09:56:28 -0500

    ajout class sensor

---
+++ src/Sensors.cpp
@@ -0,0 +1,18 @@
+/*
+ * Sensors.cpp
+ *
+ *  Created on: Feb 4, 2017
+ *      Author: XPS15
+ */
+
+#include <Sensors.h>
+
+Sensors::Sensors() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Sensors::~Sensors() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Sensors.h
@@ -0,0 +1,17 @@
+/*
+ * Sensors.h
+ *
+ *  Created on: Feb 4, 2017
+ *      Author: XPS15
+ */
+
+#ifndef SRC_SENSORS_H_
+#define SRC_SENSORS_H_
+
+class Sensors {
+public:
+	Sensors();
+	virtual ~Sensors();
+};
+
+#endif /* SRC_SENSORS_H_ */
