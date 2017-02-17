3d96246ea05e1c9d621833ba769496b2a754b317
Arezki Issaadi <arezkiissaadi@gmail.com>
2017-02-17 17:10:50 -0500

    Ajout capot, ramasseur, compresseur, shifter, pistons grimpeur

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -16,31 +16,41 @@ class Robot: public frc::IterativeRobot
 {
 	public:
 		FILE *out = 0;
-		Solenoid *capot;
 		CANTalon *ramasseur;
+		Solenoid *shifter;
+		Solenoid *grimpeurpiston;
+		DoubleSolenoid *blocker;
+		DoubleSolenoid *gear;
+		DoubleSolenoid *capot;
+		Compressor *compressor;
 
 void RobotInit()
 	{
-		sdc = new SystemesDeControle;
+		sdc=new SystemesDeControle;
 		sdc->initSystemes();
-		joyPilote = new Joystick(JOYSTICK_PortJoystickPilote);
-		modeautonome = new ModeAutonome(sdc);
+		joyPilote=new Joystick(JOYSTICK_PortJoystickPilote);
+		modeautonome=new ModeAutonome(sdc);
 		modeautonome->choose_scenario(1);
-		capot=new Solenoid(3);
-		ramasseur=new CANTalon(6);
-
+		ramasseur=new CANTalon(0);
+		shifter=new Solenoid(7);
+		grimpeurpiston=new Solenoid(6);
+		blocker=new DoubleSolenoid(0,5);
+		gear=new DoubleSolenoid(1,4);
+		capot=new DoubleSolenoid(2,3);
+		compressor=new Compressor;
+		compressor->Start();
 	}
 
 void AutonomousInit() override
 	{
-		t = 0;
+		t=0;
 	}
 
 void AutonomousPeriodic()
 	{
 		modeautonome->Execute(t);
 		sdc->Update();
-		t = t + 1;
+		t=t+1;
 	}
 
 void TeleopInit()
@@ -49,42 +59,65 @@ void TeleopInit()
 
 void TeleopPeriodic()
 	{
-		sdc->lanceur.homein();
-		//tests alignement
 
-		bool button_1=joyPilote->GetRawButton(10);
-		bool button_2=joyPilote->GetRawButton(11);
-		sdc->lanceur.mouvealign(button_1,button_2);
+// SHOOTER //////////////////////////////////////////////////////////////////////////
 
-		if (button_1==false)
+		//sdc->lanceur.homein();
+
+// CORRECTION //////////////////////////////////////////////////////////////////////
+
+		bool button1=joyPilote->GetRawButton(10);
+		bool button2=joyPilote->GetRawButton(11);
+		//sdc->lanceur.mouve_align(button1,button2);
+
+		if (button1==false)
 			{
 				sdc->basemobile.SetAngleDelta(0);
 			}
 
 		std::cout << "GYRO: " << sdc->sensors->gyro->GetAngle() << '\n';
-		std::cout << "ENCODEUR: " << sdc->sensors->encoderdrive1->GetRaw() << '\n';
 
 
-		sdc->basemobile.Drive(joyPilote->GetRawAxis(MAPPING_drivex), joyPilote->GetRawAxis(MAPPING_drivey));
+// DRIVE //////////////////////////////////////////////////////////////////////////////
 
-/////////////////////////////////////////// CAPOT
-		bool button_capot=joyPilote->GetRawButton(4);
-		if(button_capot==true) capot->Set(true);
-		else capot->Set(false);
-
-
-///////////////////////////////////RAMASSEUR DE BALLES
-
-		bool button_invert=joyPilote->GetRawButton(5);
-
-		bool button_ramasseur=joyPilote->GetRawButton(6);
-
-		if(button_ramasseur==true && button_invert==false) ramasseur->Set(1);
-		else ramasseur->Set(0);
-		if (button_invert==true && button_ramasseur==true) ramasseur->Set(-1);
-		else ramasseur->Set(0);
-
+		sdc->basemobile.Drive(joyPilote->GetRawAxis(0), joyPilote->GetRawAxis(1));
 		sdc->Update();
+
+// CAPOT ///////////////////////////////////////////////////////////////////////////////
+
+		bool button_capot=joyPilote->GetRawButton(4);
+		if(button_capot==true) capot->Set(DoubleSolenoid::Value::kForward);
+		else capot->Set(DoubleSolenoid::Value::kOff);
+
+// RAMASSEUR DE BALLES //////////////////////////////////////////////////////////////////
+
+		bool button_ramasseur=joyPilote->GetRawButton(5);
+		bool button_invert=joyPilote->GetRawButton(6);
+
+		if(button_ramasseur==true && button_invert==false)
+			{ramasseur->Set(1);}
+		else
+			{ramasseur->Set(0);}
+
+		//if (button_invert==true && button_ramasseur==true)
+			//{ramasseur->Set(-1);}
+		//else ramasseur->Set(0);
+
+
+
+// PISTONS GRIMPEUR ////////////////////////////////////////////////////////////////////////////
+
+		bool button_pistongrimpeur=joyPilote->GetRawButton(12);
+
+		if (button_pistongrimpeur==true)
+		{
+			grimpeurpiston->Set(true);
+		}
+		else
+		{
+			grimpeurpiston->Set(false);
+		}
+
 	}
 
 void TestPeriodic()
@@ -101,7 +134,6 @@ void DisabledPeriodic()
 		SystemesDeControle *sdc;
 		Joystick *joyPilote;
 		ModeAutonome *modeautonome;
-
 		CANTalon *talon;
 
 		unsigned int t = 0;
